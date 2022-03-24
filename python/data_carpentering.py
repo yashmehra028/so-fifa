d1e8bbb6c1ec31f81a10bc8af450c6cb303bb6bb
Ritvik Handa <rhanda2@huskers.unl.edu>
2022-03-23 21:18:50 -0500

    Split player skills from other details.

---
+++ python/data_carpentering.py
@@ -0,0 +1,23 @@
+from operator import index
+import pandas as pd
+
+df = pd.read_csv("./data/processed.csv")
+
+player_skills = df[['Crossing', 'Finishing', 'Heading Accuracy', 'Short Passing', 'Volleys',
+       'Dribbling', 'Curve', 'FK Accuracy', 'Long Passing', 'Ball Control',
+       'Acceleration', 'Sprint Speed', 'Agility', 'Reactions', 'Balance',
+       'Shot Power', 'Jumping', 'Stamina', 'Strength', 'Long Shots',
+       'Aggression', 'Interceptions', 'Positioning', 'Vision', 'Penalties',
+       'Composure', 'Defensive Awareness', 'Standing Tackle', 'Sliding Tackle',
+       'GK Diving', 'GK Handling', 'GK Kicking', 'GK Positioning',
+       'GK Reflexes']]
+
+player_skills.reset_index(inplace=True)
+final_player_skills = player_skills.rename(columns={"index":"Player_Id"})
+
+player_skills.to_csv("./data/player_skills_attributes.csv")
+
+player_details = df[['Name', 'Overall Rating', 'Potential', 'Value', 'Wage','Positions', 'Nationality', 'Age', 'Height', 'Weight',
+       'Primary Position', 'Club', 'National Team']]
+
+player_details.to_csv("./data/player_details.csv")
