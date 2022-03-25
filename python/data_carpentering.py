0c8ec32db60343673f4ee1648c08e46d30adc5a2
Ritvik Handa <rhanda2@huskers.unl.edu>
2022-03-25 14:38:35 -0500

    Fixed join column player id in both datasets.

--- python/data_carpentering.py
+++ python/data_carpentering.py
@@ -15,9 +15,12 @@ player_skills = df[['Crossing', 'Finishing', 'Heading Accuracy', 'Short Passing'
 player_skills.reset_index(inplace=True)
 final_player_skills = player_skills.rename(columns={"index":"Player_Id"})
 
-player_skills.to_csv("./data/player_skills_attributes.csv")
+final_player_skills.to_csv("./data/player_skills_attributes.csv")
 
 player_details = df[['Name', 'Overall Rating', 'Potential', 'Value', 'Wage','Positions', 'Nationality', 'Age', 'Height', 'Weight',
        'Primary Position', 'Club', 'National Team']]
 
-player_details.to_csv("./data/player_details.csv")
+player_details.reset_index(inplace=True)
+final_player_details = player_details.rename(columns={"index":"Player_Id"})
+
+final_player_details.to_csv("./data/player_details.csv")
