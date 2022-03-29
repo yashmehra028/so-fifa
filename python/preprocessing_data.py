8790b4c9f02a7fa5eeb4f9a13856c834b56d663f
Yash Mehra <yashmehra028@gmail.com>
2022-03-29 01:51:27 -0500

    change

--- python/preprocessing_data.py
+++ python/preprocessing_data.py
@@ -15,13 +15,13 @@ def get_positions(position_str: str) -> list:
 
 
 def get_teams(row) -> list:
-    teams = re.sub("[\[\]\']", "", row["Teams"]).split(",")
+    teams = re.sub("[\[\]\'\"]", "", row["Teams"]).split(",")
     teams = [x.strip(" ") for x in teams]
     if len(teams) == 1:
         if fuzz.partial_ratio(row["Nationality"], teams[0]) > 80:
-            return ["", teams[0]]
+            return ["No club", teams[0]]
         else:
-            return [teams[0], ""]
+            return [teams[0], "No national team"]
     if fuzz.partial_ratio(row["Nationality"], teams[0]) > fuzz.partial_ratio(row["Nationality"], teams[1]):
         return [teams[1], teams[0]]
     else:
@@ -35,11 +35,11 @@ fifa_data['Positions'] = fifa_data["Positions"].apply(get_positions)
 fifa_data['Primary Position'] = fifa_data["Positions"].apply(lambda x: x[0])
 
 fifa_data["Teams"] = fifa_data.apply(get_teams, axis=1)
-fifa_data['Teams'] = fifa_data['Teams'].apply(lambda x: x)
 fifa_data['Club'] = fifa_data['Teams'].apply(lambda x: x[0])
 fifa_data['National Team'] = fifa_data['Teams'].apply(lambda x: x[1])
 
 fifa_data.reset_index(inplace=True)
 fifa_data.drop(columns=['Teams', 'Unnamed: 0', 'index'], inplace=True)
 
+fifa_data.dropna(inplace=True)
 fifa_data.to_csv("./data/processed.csv", index=False)
